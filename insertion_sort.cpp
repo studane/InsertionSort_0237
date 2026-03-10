24235fdfff96d37ece59c4271507c68d731f5b19
studane <zhidaneprabowoguzlend@gmail.com>
2026-03-10 10:55:04 +0700

    void display

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -45,3 +45,16 @@ void insertionsort(){ //procedure insertion sort
         } 
     }
 }
+
+void display(){
+    cout << endl;
+    cout << "\n=================";
+    cout << "\nElement Array" << endl;
+    cout << "\n=================";
+
+    for(int j = 0; j < n; j++){
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
+
